2bf115ff1c23c513f6da2dcb66110240117d0583
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2020-05-13 10:35:04 +1200

    added usage

--- scripts/install/gbsh.sh
+++ scripts/install/gbsh.sh
@@ -5,4 +5,4 @@ mkdir -p ~/bin/
 echo "#!/usr/bin/env sh\n\ncurl -sL sh.geee.be/$1/$2 | sh" > ~/bin/gbsh
 chmod +x ~/bin/gbsh
 
-[[ ! `which gbsh` ]] && printf 'Please add ~/bin to your PATH:\nexport PATH=$PATH:~/bin'
+[[ ! `which gbsh` ]] && printf 'Please add ~/bin to your PATH:\nexport PATH=$PATH:~/bin' || echo 'Installed. Usage: gbsh <section> <script>'
