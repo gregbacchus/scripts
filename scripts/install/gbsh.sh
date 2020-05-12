6a35506b9bccf556d97e15ddeeaf276fc274a805
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2020-05-13 10:33:57 +1200

    add script to install gbsh

---
+++ scripts/install/gbsh.sh
@@ -0,0 +1,8 @@
+set -e
+
+mkdir -p ~/bin/
+
+echo "#!/usr/bin/env sh\n\ncurl -sL sh.geee.be/$1/$2 | sh" > ~/bin/gbsh
+chmod +x ~/bin/gbsh
+
+[[ ! `which gbsh` ]] && printf 'Please add ~/bin to your PATH:\nexport PATH=$PATH:~/bin'
