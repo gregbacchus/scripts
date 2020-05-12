f32bbba9ea912de7b0446ab844e4501906d2bf34
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2020-05-13 10:39:17 +1200

    fix: variable substitution

--- scripts/install/gbsh.sh
+++ scripts/install/gbsh.sh
@@ -2,7 +2,7 @@ set -e
 
 mkdir -p ~/bin/
 
-echo "#!/usr/bin/env sh\n\ncurl -sL sh.geee.be/$1/$2 | sh" > ~/bin/gbsh
+echo '#!/usr/bin/env sh\n\ncurl -sL sh.geee.be/$1/$2 | sh' > ~/bin/gbsh
 chmod +x ~/bin/gbsh
 
 [ ! `which gbsh` ] && printf 'Please add ~/bin to your PATH:\nexport PATH=$PATH:~/bin' || echo 'Installed. Usage: gbsh <section> <script>'
