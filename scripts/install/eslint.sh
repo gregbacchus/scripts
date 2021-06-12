32e53f07b73dc54f44a2a096cd19b3701955d0fe
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2021-06-12 20:51:35 +1200

    chore: update eslint

--- scripts/install/eslint.sh
+++ scripts/install/eslint.sh
@@ -1,3 +1,3 @@
 set -e
 
-npm install -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @typescript-eslint/eslint-plugin-tslint eslint-plugin-prefer-arrow
+npm install -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @typescript-eslint/eslint-plugin-tslint eslint-plugin-prefer-arrow npm install eslint-plugin-filenames
