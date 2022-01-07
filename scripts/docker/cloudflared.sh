b91a440387088b4acb210b485aad296351dd6c2b
Greg Bacchus <greg.bacchus@directed.co.nz>
2022-01-07 17:03:34 +1300

    chore: cloudflared

---
+++ scripts/docker/cloudflared.sh
@@ -0,0 +1,38 @@
+set -e
+
+sudo mkdir -p /etc/cloudflared
+sudo chown -R 65532:root /etc/cloudflared
+
+function cf_login {
+  sudo docker run \
+    -it --rm \
+    --network host \
+    -v /etc/cloudflared:/home/nonroot/.cloudflared \
+    cloudflare/cloudflared:2022.1.0 \
+    tunnel login
+}
+
+function cf_exec {
+  sudo docker run \
+    -it --rm \
+    --network host \
+    -v /etc/cloudflared:/home/nonroot/.cloudflared \
+    cloudflare/cloudflared:2022.1.0 \
+    $@
+}
+
+[[ ! -f /etc/cloudflared/cert.pem ]] && cf_login || :
+
+[[ -z $* ]] && cf_exec $@
+
+sudo docker stop cloudflared || :
+sudo docker rm cloudflared || :
+
+sudo docker run \
+  --name cloudflared \
+  -d \
+  --restart always \
+  --network host \
+  -v /etc/cloudflared:/home/nonroot/.cloudflared \
+  cloudflare/cloudflared:2022.1.0 \
+  tunnel run
