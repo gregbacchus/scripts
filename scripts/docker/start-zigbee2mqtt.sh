d4baa5f76222b769cc77250fd15adc738e2b1f97
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2023-03-05 15:00:17 +1300

    run in background

--- scripts/docker/start-zigbee2mqtt.sh
+++ scripts/docker/start-zigbee2mqtt.sh
@@ -7,7 +7,8 @@ sudo docker pull koenkk/zigbee2mqtt:latest
 sudo docker stop zigbee2mqtt || :
 sudo docker rm zigbee2mqtt || :
 
-docker run --name zigbee2mqtt \
+sudo docker run --name zigbee2mqtt \
+  -d \
   --restart always \
   --device /dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_7cf686328218ec119484e99a47486eb0-if00-port0:/dev/ttyACM0 \
   -v zigbee2mqtt:/app/data \
