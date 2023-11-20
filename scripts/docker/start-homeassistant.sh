867ac5c5fee0ccb34837fe6d8e2a47bd3fe6925d
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2023-11-20 16:00:15 +1300

    Update start-homeassistant.sh

--- scripts/docker/start-homeassistant.sh
+++ scripts/docker/start-homeassistant.sh
@@ -12,7 +12,6 @@ sudo docker run --name homeassistant \
   --restart always \
   -v /data/homeassistant:/config \
   -v /etc/localtime:/etc/localtime:ro \
-  --device /dev/ttyUSB0:/dev/ttyUSB0 \
   --device /dev/ttyUSB1:/dev/ttyUSB1 \
   -e TZ=Pacific/Auckland \
   --net host \
